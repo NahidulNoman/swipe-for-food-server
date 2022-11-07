b736bfe4e405c14e01ec7db506d1c70b1cbbafb7
NahidulNoman <nahidulna5@gmail.com>
2022-11-08 00:11:01 +0600

    three service added for home page

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
-// middle wares 
+// middle wares
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('swipe for food server is running...')
+app.get("/", (req, res) => {
+  res.send("swipe for food server is running...");
 });
 
-app.listen(port , ()=> {
-    console.log(`swipe for food server is running port ${port}`)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a31ucvz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try{
+        const serviceCollection = client.db("swipeForFood").collection("foodServices");
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error));
+
+
+app.listen(port, () => {
+  console.log(`swipe for food server is running port ${port}`);
 });
